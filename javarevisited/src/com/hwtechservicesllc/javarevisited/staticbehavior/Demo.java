4a566477e59ccefa70ad60d55f67a3b05c94cb15
hoiwanjohnlouis@gmail.com <hoiwanjohnlouis@gmail.com>
2015-10-29 01:33:12 -0400

    initial commit for module (javarevisited)

---
+++ javarevisited/src/com/hwtechservicesllc/javarevisited/staticbehavior/Demo.java
@@ -0,0 +1,33 @@
+package com.hwtechservicesllc.javarevisited.staticbehavior;
+
+/**
+ * Copyright 8/22/2015 by HW Tech Services, Inc., LLC
+ * <p>
+ * Package com.hwtechservices.javarevisited.staticbehavior
+ * Project staticbehavior
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+public class Demo {
+    public static void main(String args[]){
+        Vehicle v = new Car();
+        v.kmToMiles(10);
+        StaticVehicle v2 = new StaticCar();
+        v2.kmToMiles(50);
+    }
+}
+// Output: static clause affect inheritance behavior as seen below
+//    Inside Child class' non-static method 10
+//    Inside parent class' static method 50
+
---
+++ javarevisited/src/com/hwtechservicesllc/javarevisited/threadstopdemo/Main.java
@@ -0,0 +1,25 @@
+package com.hwtechservicesllc.javarevisited.threadstopdemo;
+
+import java.util.concurrent.TimeUnit;
+import static java.lang.Thread.currentThread;
+
+public class Main {
+
+    public static void main(String args[]) throws InterruptedException {
+
+        ThreadServer myServer = new ThreadServer();
+        Thread t1 = new Thread(myServer, "T1");
+        t1.start();
+
+        //Let the server thread run a while
+        // TimeUnit.MILLISECONDS.sleep(1);
+
+        //Now, let's stop our Server thread
+        System.out.println(currentThread().getName() + " is stopping Server thread");
+        myServer.stop();
+
+        //Let's wait to see server thread stopped
+        TimeUnit.MILLISECONDS.sleep(200);
+        System.out.println(currentThread().getName() + " is finished now");
+    }
+}
