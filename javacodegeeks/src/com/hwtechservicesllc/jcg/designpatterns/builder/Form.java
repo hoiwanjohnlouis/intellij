d2c912af1a296c70e5d4f888bbe8f3f36b0bf6fb
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-14 06:52:57 -0500

    updated  module  javacodegeeks/designpatterns/builder.  created TestForm class using "main" code extracted from Form class.

--- javacodegeeks/src/com/hwtechservicesllc/jcg/designpatterns/builder/Form.java
+++ javacodegeeks/src/com/hwtechservicesllc/jcg/designpatterns/builder/Form.java
@@ -196,9 +196,4 @@ public class Form {
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        Form form = new Form.FormBuilder("Dave", "Carter", "DavCarter", "DAvCaEr123").passwordHint("MyName").city("NY").language("English").build();
-        System.out.println(form);
-    }
-
 }
