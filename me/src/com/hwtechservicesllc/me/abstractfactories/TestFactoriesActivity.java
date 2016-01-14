c39ddb8ddfb275357471ab86199cb18e61ee344b
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-14 06:27:26 -0500

    shortened package names in mystuff to save on typing.

---
+++ me/src/com/hwtechservicesllc/me/abstractfactories/TestFactoriesActivity.java
@@ -0,0 +1,25 @@
+package com.hwtechservicesllc.me.abstractfactories;
+
+
+import com.hwtechservicesllc.me.abstractfactories.factories.Record;
+import com.hwtechservicesllc.me.abstractfactories.factories.RecordBuilding;
+import com.hwtechservicesllc.me.abstractfactories.factories.TradeBuilding;
+import com.hwtechservicesllc.me.abstractfactories.fields.ERecordType;
+
+public class TestFactoriesActivity {
+
+    // for logging purposes
+    private final String DEBUG_TAG = this.getClass().getSimpleName();
+
+
+    public static void main (String[] args) {
+
+        //  testing ticker record construction
+        RecordBuilding createTrade = new TradeBuilding();
+        Record record = createTrade.orderRecord(ERecordType.TRADE_RECORD);
+        System.out.println(record + "\n");
+
+
+    }
+
+}
--- mystuff/src/com/hwtechservicesllc/mystuff/abstractfactories/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hwtechservicesllc.mystuff.abstractfactories;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
