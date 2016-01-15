93097caac056d0fc11e8a489ca98a6403e89df5c
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-15 04:30:55 -0500

    updated javarevisited (20160115-1)

---
+++ javarevisited/src/com/hwtechservicesllc/jr/looppuzzlefail1/TestForEach.java
@@ -0,0 +1,39 @@
+package com.hwtechservicesllc.jr.looppuzzlefail1;
+
+/**
+ * Copyright 8/16/2015 by HW Tech Services, Inc., LLC
+ * <p>
+ * Package com.hwtechservices.jr.looppuzzlefail1
+ * Project looppuzzlefail1
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+/**
+ * Java Class to show how for-each loop works in Java
+ */
+public class TestForEach {
+
+    public static void main(String args[]){
+        CustomCollection<String> myCollection = new CustomCollection<String>();
+        myCollection.add("Java");
+        myCollection.add("Scala");
+        myCollection.add("Groovy");
+
+        // What does this code will do, print language,
+        // throw exception or compile time error
+        for(String language: myCollection){
+            System.out.println(language);
+        }
+    }
+}
