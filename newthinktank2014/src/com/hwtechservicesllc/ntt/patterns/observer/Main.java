97ad35b38cb99b01d1fe5279faffd18ca9cd6d93
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-28 15:23:02 -0500

    updated intellijPrototyping project (20160128-6), module newthinktank2014 removed hwtechservicesllc from package name to save on typing

--- newthinktank2014/src/com/hwtechservicesllc/ntt/patterns/observer/Main.java
+++
@@ -1,8 +0,0 @@
-package com.hwtechservicesllc.ntt.patterns.observer;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ newthinktank2014/src/com/ntt2014/patterns/observer/Main.java
@@ -0,0 +1,8 @@
+package com.ntt2014.patterns.observer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
