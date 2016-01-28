af302c077fa4eb85d695eb47b513ffe99af00101
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-28 15:19:54 -0500

    updated intellijPrototyping project (20160128-4), module javacodegeeks, removed hwtechservicesllc from package name to save on typing.

--- javacodegeeks/src/com/hwtechservicesllc/jcg/designpatterns/abstractfactory/XMLParser.java
+++
@@ -1,7 +0,0 @@
-package com.hwtechservicesllc.jcg.designpatterns.abstractfactory;
-
-public interface XMLParser {
-	
-	public String parse();
-
-}
--- javacodegeeks/src/com/hwtechservicesllc/jcg/designpatterns/chainofresponsibility/Handler.java
+++
@@ -1,8 +0,0 @@
-package com.hwtechservicesllc.jcg.designpatterns.chainofresponsibility;
-
-public interface Handler {
-	
-	public void setHandler(Handler handler);
-	public void process(File file);
-	public String getHandlerName();
-}
