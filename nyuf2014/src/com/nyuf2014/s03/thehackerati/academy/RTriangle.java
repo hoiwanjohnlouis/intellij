514c54196444aa63b206fee2178fff11036da473
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-01-29 18:04:31 -0500

    nyuf2014 project (20160129-2), uploaded remaining source, test, and lib folders.  uploaded selected files from .idea folder

---
+++ nyuf2014/src/com/nyuf2014/s03/thehackerati/academy/RTriangle.java
@@ -0,0 +1,15 @@
+package com.nyuf2014.s03.thehackerati.academy;
+
+public class RTriangle extends Shape {
+    public double base;
+    public double height;
+
+    public RTriangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
---
+++ nyuf2014/test/com/nyuf2014/s03/thehackerati/academy/ProcessFiles2Test.java
@@ -0,0 +1,33 @@
+package com.nyuf2014.s03.thehackerati.academy;
+
+/**
+ * ************************************************************************
+ * Copyright 01/28/2016 HW Tech Services, LLC
+ * <p>
+ * Login   KimSoYeon
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * *************************************************************************
+ */
+
+public class ProcessFiles2Test {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Starting Session03\\ProcessFiles2Test");
+        ProcessFiles2.processFiles2();
+        System.out.println("Ending Session03\\ProcessFiles2Test");
+
+    }
+
+}
