670515d19e5ff3f9a9ec62d8d34507577a22f701
hoiwanjohnlouis@gmail.com <Iyjnuofs637_!>
2016-02-15 08:06:25 -0500

    (20160215-nyu-20) added s03thehackerati project to nyu for GIT

---
+++ nyu/s03thehackerati/src/main/java/com/nyuf2014/s03thehackerati/shapes/RTriangle.java
@@ -0,0 +1,15 @@
+package com.nyuf2014.s03thehackerati.shapes;
+
+public class RTriangle extends Shape {
+    public double base;
+    public double height;
+
+    public RTriangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
