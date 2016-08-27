0d0f73b82ec0998188e1c8f4f4ac6259a72eaaa9
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2016-08-27 19:48:28 -0400

    reconfigured packages

--- tij4/ch08polymorphism/src/main/java/com/tij4f2014/ch08/Ex01Cycle.java
+++
@@ -1,7 +0,0 @@
-package com.tij4f2014.ch08;
-
-/**
- * Created by callas on 9/16/2014.
- */
-public class Ex01Cycle {
-}
--- tij4/ch18javaiosystem/src/main/java/com/tij4f2014/ch18/Main.java
+++
@@ -1,8 +0,0 @@
-package com.tij4f2014.ch18;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
