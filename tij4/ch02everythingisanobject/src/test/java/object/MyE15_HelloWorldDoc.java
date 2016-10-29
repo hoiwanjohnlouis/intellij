6d326e7be3553338c76afdfbee2fe45960a3f73a
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2016-10-28 22:42:41 -0400

    (20161028-1) updated idea/intellijPrototyping/tij4/ch02everythingisanobject.  2. Added remaining example stubs

---
+++ tij4/ch02everythingisanobject/src/test/java/object/MyE15_HelloWorldDoc.java
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Copyright 2016 HW Tech Services, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author KimSoYeon
+ * @param args none
+ * @return none
+ * @throws none
+ ***********************************************************************/
+package object;
+
+public class MyE15_HelloWorldDoc {
+}
