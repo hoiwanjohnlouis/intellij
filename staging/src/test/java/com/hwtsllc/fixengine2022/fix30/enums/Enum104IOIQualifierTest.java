e7d8feeb43a4e1080414cc9b57811eb2362316fc
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-03-26 11:21:35 -0400

    (20220326-112100) 1. Removed BeforeEach and AfterEach from FIX30 EnumnnnnTest routines. Not being used.

--- staging/src/test/java/com/hwtsllc/fixengine2022/fix30/enums/Enum104IOIQualifierTest.java
+++ staging/src/test/java/com/hwtsllc/fixengine2022/fix30/enums/Enum104IOIQualifierTest.java
@@ -23,22 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class Enum104IOIQualifierTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-    }
-
-    @AfterAll
-    static void afterAll() {
-    }
-
     @Test
     void Enum0104Test() {
         Enum104IOIQualifier enumType;
