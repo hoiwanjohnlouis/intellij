74ef96088f1eed87eda6f0c1db53b6034a00acf1
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-03-27 18:01:42 -0400

    (20220327-180000) 1. Removed BeforeEach and AfterEach from FIX44 TagnnnnTest routines.

--- staging/src/test/java/com/hwtsllc/fixengine2022/fix44/tags/Tag876CPRegTypeTest.java
+++ staging/src/test/java/com/hwtsllc/fixengine2022/fix44/tags/Tag876CPRegTypeTest.java
@@ -17,36 +17,17 @@
 package com.hwtsllc.fixengine2022.fix44.tags;
 
 import com.hwtsllc.fixengine2022.datatypes.FIXType;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class Tag876CPRegTypeTest {
-    private final String WHERE_AM_I = this.getClass().getSimpleName();
-    private static final Logger logger = LogManager.getRootLogger();
-    // private static final Logger logger = LogManager.getLogger(Tag1StAccount.class);
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void Test() {
-        assertEquals(1,1);
-        assertNotEquals(2,1);
-        logger.info(WHERE_AM_I + ":Successful Test(). More testing is needed.");
-    }
     @Test
     void FIX0876Test() {
         FIXType fixData = FIXType.CP_REG_TYPE;
+        assertNotEquals( FIXType.JUNK_ENUM_NAME, fixData.getEnumName());
+        assertNotEquals( FIXType.JUNK_ID, fixData.getID());
+        assertNotEquals( FIXType.JUNK_NAME, fixData.getName());
+        assertNotEquals( FIXType.JUNK_DESCRIPTION, fixData.getDescription());
     }
 }
