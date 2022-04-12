fceb148b31a90bc1d4d53bce657ed02691a33d3c
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-04-12 15:56:41 -0400

    (20220412-155100) 1. FIX42 defined INT (int primative type) tag types.

---
+++ staging/src/main/java/com/hwtsllc/fixengine2022/fix42/tags/Tag244IntUnderlyingRepurchaseTerm.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2022.  HW Tech Services, LLC
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.hwtsllc.fixengine2022.fix42.tags;
+
+import com.hwtsllc.fixengine2022.datatypes.FIX42;
+import com.hwtsllc.fixengine2022.datatypes.FIX42Abstract;
+import com.hwtsllc.fixengine2022.datatypes.MyIntType;
+import com.hwtsllc.fixengine2022.interfaces.FixTagValuePairString;
+import com.hwtsllc.fixengine2022.interfaces.LogStringVerbose;
+
+@Deprecated
+public class Tag244IntUnderlyingRepurchaseTerm extends FIX42Abstract implements FixTagValuePairString, LogStringVerbose {
+    private final MyIntType dataValue;
+
+    public final static int TESTA_INT_UNDERLYING_REPURCHASE_TERM = 60;
+    public final static int TESTB_INT_UNDERLYING_REPURCHASE_TERM = 30;
+
+    public Tag244IntUnderlyingRepurchaseTerm(MyIntType dataValue) {
+        setFixType(FIX42.FIX244_INT_UNDERLYING_REPURCHASE_TERM);
+        this.dataValue = dataValue;
+    }
+
+    public int getDataValue() {
+        return this.dataValue.getDataValue();
+    }
+    /**
+     * standard wrapper to retrieve the build a standard fix message for this tag
+     */
+    @Override
+    public String toFixTagValuePairString() {
+        return getID()
+                .concat("=")
+                .concat(dataValue.toString());
+    }
+    /**
+     * standard wrapper to format a detailed string describing this data field
+     */
+    @Override
+    public String toLogStringVerbose() {
+        return super.toLogStringVerbose()
+                .concat("\n\tDataValue[")
+                .concat(toString())
+                .concat("]");
+    }
+    /**
+     * standard wrapper to format a simple string describing the data
+     */
+    @Override
+    public String toString() {
+        return String.valueOf(getDataValue());
+    }
+
+    /**
+     *
+     * @param args   no args used at this time
+     */
+    public static void main(String[] args) {
+        Tag244IntUnderlyingRepurchaseTerm tagData;
+
+        tagData = new Tag244IntUnderlyingRepurchaseTerm(new MyIntType(TESTA_INT_UNDERLYING_REPURCHASE_TERM) );
+        System.out.println(tagData);
+        System.out.println(tagData.toLogStringVerbose());
+        System.out.println(tagData.toFixTagValuePairString());
+
+        tagData = new Tag244IntUnderlyingRepurchaseTerm(new MyIntType(TESTB_INT_UNDERLYING_REPURCHASE_TERM) );
+        System.out.println(tagData);
+        System.out.println(tagData.toLogStringVerbose());
+        System.out.println(tagData.toFixTagValuePairString());
+    }
+}
